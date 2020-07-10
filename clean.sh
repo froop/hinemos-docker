0473836dc0fee24a8013d8baa5c8f7b3bc6fbff5
froop <773445+froop@users.noreply.github.com>
2020-07-10 19:34:36 +0900

    rename hinemos-*-base from *-base

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker-compose down --rmi all
-docker rmi web-base
-docker rmi manager-base
+docker rmi hinemos-web-base
+docker rmi hinemos-manager-base
 docker rmi openjdk-centos7:8
 docker rmi centos7jp
