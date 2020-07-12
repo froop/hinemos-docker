44abb18d26003146900fe957c32b4346a005e1fd
froop <773445+froop@users.noreply.github.com>
2020-07-12 11:02:01 +0900

    clean agent images

--- clean.sh
+++ clean.sh
@@ -2,8 +2,10 @@
 
 docker-compose down --rmi all
 docker rmi hinemos-manager:6.2.2
+docker rmi hinemos-agent:6.2.2
 docker rmi hinemos-web:6.2.2
-docker rmi hinemos-web-base
 docker rmi hinemos-manager-base
+docker rmi hinemos-agent-base
+docker rmi hinemos-web-base
 docker rmi openjdk-centos7:8
 docker rmi centos7jp
