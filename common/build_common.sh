e9a3f73f0b7fdd6f246e6810d5046284f62d082b
froop <773445+froop@users.noreply.github.com>
2020-07-13 00:32:52 +0900

    move base/${SERVICE}-base to ${SERVICE}/base move base/${SERVICE}-package to ${SERVICE}/package

--- common/build_common.sh
+++ common/build_common.sh
@@ -5,13 +5,13 @@ SERVICE=$1
 export HINEMOS_VER=${HINMEOS_MAJOR}.${HINEMOS_MINOR}
 export JRE_TAG=openjdk${JAVA_VER}-${OS}
 
-docker build -t hinemos-${SERVICE}-base:${JRE_TAG} base/${SERVICE}-base --build-arg FROM=openjdk-${OS}:${JAVA_VER}
+docker build -t hinemos-${SERVICE}-base:${JRE_TAG} ${SERVICE}/base --build-arg FROM=openjdk-${OS}:${JAVA_VER}
 status=$?
 if [ $status -ne 0 ]; then
   exit $status
 fi
 
-docker build -t hinemos-${SERVICE}-${HINEMOS_VER}:${JRE_TAG} base/${SERVICE}-package --build-arg FROM=hinemos-${SERVICE}-base:openjdk${JAVA_VER}-${OS} --build-arg MAJOR=${HINMEOS_MAJOR} --build-arg MINOR=${HINEMOS_MINOR}
+docker build -t hinemos-${SERVICE}-${HINEMOS_VER}:${JRE_TAG} ${SERVICE}/package --build-arg FROM=hinemos-${SERVICE}-base:openjdk${JAVA_VER}-${OS} --build-arg MAJOR=${HINMEOS_MAJOR} --build-arg MINOR=${HINEMOS_MINOR}
 status=$?
 if [ $status -ne 0 ]; then
   exit $status
