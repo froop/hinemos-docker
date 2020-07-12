fb19f5825a0121de2d4ae0c42c17b3b254c2a1d8
froop <773445+froop@users.noreply.github.com>
2020-07-12 23:33:35 +0900

    hinemos-${SERVICE}-${HINEMOS_VER}:${JRE_TAG}

--- clean.sh
+++ clean.sh
@@ -9,9 +9,9 @@ JRE_TAG=openjdk${JAVA_VER}-${OS}
 HINEMOS_VER=${HINMEOS_MAJOR}.${HINEMOS_MINOR}
 
 docker-compose down --rmi all
-docker rmi hinemos-manager:${HINEMOS_VER}-${JRE_TAG}
-docker rmi hinemos-agent:${HINEMOS_VER}-${JRE_TAG}
-docker rmi hinemos-web:${HINEMOS_VER}-${JRE_TAG}
+docker rmi hinemos-manager-${HINEMOS_VER}:${JRE_TAG}
+docker rmi hinemos-agent-${HINEMOS_VER}:${JRE_TAG}
+docker rmi hinemos-web-${HINEMOS_VER}:${JRE_TAG}
 docker rmi hinemos-manager-base:${JRE_TAG}
 docker rmi hinemos-agent-base:${JRE_TAG}
 docker rmi hinemos-web-base:${JRE_TAG}
