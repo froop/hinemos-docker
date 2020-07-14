caa5769559b395837f1249ba76a20cd687958cd2
froop <773445+froop@users.noreply.github.com>
2020-07-15 08:18:29 +0900

    pull up db start/stop

--- manager/package/initdb/initdb.sh
+++ manager/package/initdb/initdb.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+su hinemos -c '/opt/hinemos/postgresql/bin/pg_ctl start -w -t 90 -s -D /opt/hinemos/var/data -l /opt/hinemos/var/log/postgresql.log'
+status=$?
+if [ $status -ne 0 ]; then
+  exit $status
+fi
+
 ./run_sql.sh delete_SNMPTRAP_DEFAULT.sql
+status=$?
+if [ $status -ne 0 ]; then
+  exit $status
+fi
+
+su hinemos -c '/opt/hinemos/postgresql/bin/pg_ctl stop -w -t 90 -s -D /opt/hinemos/var/data -m fast'
 status=$?
 if [ $status -ne 0 ]; then
   exit $status
--- manager/package/initdb/run_sql.sh
+++ manager/package/initdb/run_sql.sh
@@ -2,19 +2,7 @@
 
 sql_file=$1
 
-su hinemos -c '/opt/hinemos/postgresql/bin/pg_ctl start -w -t 90 -s -D /opt/hinemos/var/data -l /opt/hinemos/var/log/postgresql.log'
-status=$?
-if [ $status -ne 0 ]; then
-  exit $status
-fi
-
 PGPASSWORD=hinemos /opt/hinemos/postgresql/bin/psql -p 24001 --set ON_ERROR_STOP=on -U hinemos -d hinemos -f $sql_file
-status=$?
-if [ $status -ne 0 ]; then
-  exit $status
-fi
-
-su hinemos -c '/opt/hinemos/postgresql/bin/pg_ctl stop -w -t 90 -s -D /opt/hinemos/var/data -m fast'
 status=$?
 if [ $status -ne 0 ]; then
   exit $status
