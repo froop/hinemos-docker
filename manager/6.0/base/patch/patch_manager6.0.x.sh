18c73e505e040f89bb20765ed2c133fc4f34503c
froop <773445+froop@users.noreply.github.com>
2020-07-15 07:39:58 +0900

    add manager/common/Dockerfile

--- manager/6.0/base/patch/patch_manager6.0.x.sh
+++ manager/6.0/base/patch/patch_manager6.0.x.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-su hinemos -c '/opt/hinemos/postgresql/bin/pg_ctl start -w -t 90 -s -D /opt/hinemos/var/data -l /opt/hinemos/var/log/postgresql.log'
-status=$?
-if [ $status -ne 0 ]; then
-  exit $status
-fi
-
 /opt/hinemos/sbin/mng/hinemos_change_listen_address.sh 0.0.0.0
-status=$?
-if [ $status -ne 0 ]; then
-  exit $status
-fi
-
-su hinemos -c '/opt/hinemos/postgresql/bin/pg_ctl stop -w -t 90 -s -D /opt/hinemos/var/data -m fast'
 status=$?
 if [ $status -ne 0 ]; then
   exit $status
