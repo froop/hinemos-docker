0daac201e72421a75eba65abd347bdb8ef6fc30d
froop <773445+froop@users.noreply.github.com>
2020-07-14 20:17:58 +0900

    extract hinemos-${SERVICE}-${HINEMOS_MAJOR}.x

--- common/build_common.sh
+++ common/build_common.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 SERVICE=$1
-
-HINEMOS_VER=${HINEMOS_MAJOR}.${HINEMOS_MINOR}
 JRE_TAG=openjdk${JAVA_VER}-${OS}
 
 docker build -t hinemos-${SERVICE}-base:${JRE_TAG} ${SERVICE}/base --build-arg FROM=openjdk-${OS}:${JAVA_VER}
@@ -11,7 +9,13 @@ if [ $status -ne 0 ]; then
   exit $status
 fi
 
-docker build -t hinemos-${SERVICE}-${HINEMOS_VER}:${JRE_TAG} ${SERVICE}/package --build-arg FROM=hinemos-${SERVICE}-base:openjdk${JAVA_VER}-${OS} --build-arg MAJOR=${HINEMOS_MAJOR} --build-arg MINOR=${HINEMOS_MINOR}
+docker build -t hinemos-${SERVICE}-${HINEMOS_MAJOR}.x:${JRE_TAG} ${SERVICE}/${HINEMOS_MAJOR}/base --build-arg FROM=hinemos-${SERVICE}-base:${JRE_TAG}
+status=$?
+if [ $status -ne 0 ]; then
+  exit $status
+fi
+
+docker build -t hinemos-${SERVICE}-${HINEMOS_MAJOR}.${HINEMOS_MINOR}:${JRE_TAG} ${SERVICE}/package --build-arg FROM=hinemos-${SERVICE}-${HINEMOS_MAJOR}.x:openjdk${JAVA_VER}-${OS} --build-arg MAJOR=${HINEMOS_MAJOR} --build-arg MINOR=${HINEMOS_MINOR}
 status=$?
 if [ $status -ne 0 ]; then
   exit $status
--- common/clean_common.sh
+++ common/clean_common.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 SERVICE=$1
-
-HINEMOS_VER=${HINEMOS_MAJOR}.${HINEMOS_MINOR}
 JRE_TAG=openjdk${JAVA_VER}-${OS}
 
-docker rmi hinemos-${SERVICE}-${HINEMOS_VER}:${HINEMOS_TAG}
-docker rmi hinemos-${SERVICE}-${HINEMOS_VER}:${JRE_TAG}
+docker rmi hinemos-${SERVICE}-${HINEMOS_MAJOR}.${HINEMOS_MINOR}:${HINEMOS_TAG}
+docker rmi hinemos-${SERVICE}-${HINEMOS_MAJOR}.${HINEMOS_MINOR}{JRE_TAG}
+docker rmi hinemos-${SERVICE}-${HINEMOS_MAJOR}.x:${JRE_TAG}
 docker rmi hinemos-${SERVICE}-base:${JRE_TAG}
