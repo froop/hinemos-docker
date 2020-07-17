36645e9112d15b68d0063e7609b99e2e193ae518
froop <773445+froop@users.noreply.github.com>
2020-07-18 08:09:38 +0900

    refactor format

--- base/build.sh
+++ base/build.sh
@@ -4,6 +4,9 @@
 
 docker build -t ${OS} base/${OS}
 
-docker build -t openjdk-${OS}:${JAVA_VER} base/openjdk-redhat --build-arg VERSION=${JAVA_VER} --build-arg OS=${OS}
+docker build -t openjdk-${OS}:${JAVA_VER} base/openjdk-redhat \
+	--build-arg VERSION=${JAVA_VER} \
+	--build-arg OS=${OS}
 
-docker build -t hinemos-base:openjdk${JAVA_VER}-${OS} base/hinemos-base --build-arg FROM=openjdk-${OS}:${JAVA_VER}
+docker build -t hinemos-base:openjdk${JAVA_VER}-${OS} base/hinemos-base \
+	--build-arg FROM=openjdk-${OS}:${JAVA_VER}
