ccef36b253e58c0b44199b1ef437245d7880662b
froop <773445+froop@users.noreply.github.com>
2020-07-19 20:46:08 +0900

    refactor Dockerfile "FROM" parameter

--- base/build.sh
+++ base/build.sh
@@ -9,14 +9,15 @@ echo ${SRC_DIR} "->" ${DST_TAG}
 echo ================================================================================
 docker build -t ${DST_TAG} ${SRC_DIR}
 
+FROM_TAG=${DST_TAG}
 SRC_DIR=base/openjdk-redhat
 DST_TAG=openjdk-${OS}:${JAVA_VER}
 echo ================================================================================
 echo ${SRC_DIR} "->" ${DST_TAG}
 echo ================================================================================
 docker build -t ${DST_TAG} ${SRC_DIR} \
-	--build-arg VERSION=${JAVA_VER} \
-	--build-arg OS=${OS}
+	--build-arg FROM=${FROM_TAG} \
+	--build-arg VERSION=${JAVA_VER}
 
 FROM_TAG=${DST_TAG}
 SRC_DIR=base/hinemos-base
