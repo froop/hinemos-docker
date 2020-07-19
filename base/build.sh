0001e49455c1665ce1fce121afddfb802e275b00
froop <773445+froop@users.noreply.github.com>
2020-07-19 18:39:25 +0900

    add build log separator

--- base/build.sh
+++ base/build.sh
@@ -2,11 +2,27 @@
 
 . .env
 
-docker build -t ${OS} base/${OS}
+SRC_DIR=base/${OS}
+DST_TAG=${OS}:latest
+echo ================================================================================
+echo ${SRC_DIR} "->" ${DST_TAG}
+echo ================================================================================
+docker build -t ${DST_TAG} ${SRC_DIR}
 
-docker build -t openjdk-${OS}:${JAVA_VER} base/openjdk-redhat \
+SRC_DIR=base/openjdk-redhat
+DST_TAG=openjdk-${OS}:${JAVA_VER}
+echo ================================================================================
+echo ${SRC_DIR} "->" ${DST_TAG}
+echo ================================================================================
+docker build -t ${DST_TAG} ${SRC_DIR} \
 	--build-arg VERSION=${JAVA_VER} \
 	--build-arg OS=${OS}
 
-docker build -t hinemos-base:openjdk${JAVA_VER}-${OS} base/hinemos-base \
-	--build-arg FROM=openjdk-${OS}:${JAVA_VER}
+FROM_TAG=${DST_TAG}
+SRC_DIR=base/hinemos-base
+DST_TAG=hinemos-base:openjdk${JAVA_VER}-${OS}
+echo ================================================================================
+echo ${SRC_DIR} "->" ${DST_TAG}
+echo ================================================================================
+docker build -t ${DST_TAG} ${SRC_DIR} \
+	--build-arg FROM=${FROM_TAG}
