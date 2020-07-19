95daa1049f4e0850442163988d8c972e1e3a04e1
froop <773445+froop@users.noreply.github.com>
2020-07-19 18:52:34 +0900

    add build log separator

--- common/build_common.sh
+++ common/build_common.sh
@@ -3,17 +3,32 @@
 SERVICE=$1
 JRE_TAG=openjdk${JAVA_VER}-${OS}
 
-docker build -t hinemos-${SERVICE}-base:${JRE_TAG} \
-	${SERVICE}/base \
-	--build-arg FROM=hinemos-base:openjdk${JAVA_VER}-${OS}
+FROM_TAG=hinemos-base:openjdk${JAVA_VER}-${OS}
+SRC_DIR=${SERVICE}/base
+DST_TAG=hinemos-${SERVICE}-base:${JRE_TAG}
+echo ================================================================================
+echo ${SRC_DIR} "->" ${DST_TAG}
+echo ================================================================================
+docker build -t ${DST_TAG} ${SRC_DIR} \
+	--build-arg FROM=${FROM_TAG}
 
-docker build -t hinemos-${SERVICE}-base-${HINEMOS_MAJOR}:${JRE_TAG} \
-	${SERVICE}/${HINEMOS_MAJOR}/base \
-	--build-arg FROM=hinemos-${SERVICE}-base:${JRE_TAG}
+FROM_TAG=${DST_TAG}
+SRC_DIR=${SERVICE}/${HINEMOS_MAJOR}/base
+DST_TAG=hinemos-${SERVICE}-base-${HINEMOS_MAJOR}:${JRE_TAG}
+echo ================================================================================
+echo ${SRC_DIR} "->" ${DST_TAG}
+echo ================================================================================
+docker build -t ${DST_TAG} ${SRC_DIR} \
+        --build-arg FROM=${FROM_TAG}
 
-docker build -t hinemos-${SERVICE}-base-${HINEMOS_MAJOR}.${HINEMOS_MINOR}:${JRE_TAG} \
-	${SERVICE}/common/package \
-	--build-arg FROM=hinemos-${SERVICE}-base-${HINEMOS_MAJOR}:openjdk${JAVA_VER}-${OS} \
+FROM_TAG=${DST_TAG}
+SRC_DIR=${SERVICE}/common/package
+DST_TAG=hinemos-${SERVICE}-base-${HINEMOS_MAJOR}.${HINEMOS_MINOR}:${JRE_TAG}
+echo ================================================================================
+echo ${SRC_DIR} "->" ${DST_TAG}
+echo ================================================================================
+docker build -t ${DST_TAG} ${SRC_DIR} \
+	--build-arg FROM=${FROM_TAG} \
 	--build-arg MAJOR=${HINEMOS_MAJOR} \
 	--build-arg MINOR=${HINEMOS_MINOR}
 
