7b5590db33745d3f2e61b4aefeeec88bc08d82fe
froop <773445+froop@users.noreply.github.com>
2020-07-21 08:27:27 +0900

    rename SRC_DIR -> CONTEXT

--- common/build_common.sh
+++ common/build_common.sh
@@ -4,30 +4,30 @@ SERVICE=$1
 JRE_TAG=openjdk${JAVA_VER}-${OS}
 
 FROM_TAG=hinemos-base:openjdk${JAVA_VER}-${OS}
-SRC_DIR=${SERVICE}/base
+CONTEXT=${SERVICE}/base
 DST_TAG=hinemos-${SERVICE}-base:${JRE_TAG}
 echo ================================================================================
-echo ${SRC_DIR} "->" ${DST_TAG}
+echo ${CONTEXT} "->" ${DST_TAG}
 echo ================================================================================
-docker build -t ${DST_TAG} ${SRC_DIR} \
+docker build -t ${DST_TAG} ${CONTEXT} \
 	--build-arg FROM=${FROM_TAG}
 
 FROM_TAG=${DST_TAG}
-SRC_DIR=${SERVICE}/${HINEMOS_MAJOR}/base
+CONTEXT=${SERVICE}/${HINEMOS_MAJOR}/base
 DST_TAG=hinemos-${SERVICE}-base-${HINEMOS_MAJOR}:${JRE_TAG}
 echo ================================================================================
-echo ${SRC_DIR} "->" ${DST_TAG}
+echo ${CONTEXT} "->" ${DST_TAG}
 echo ================================================================================
-docker build -t ${DST_TAG} ${SRC_DIR} \
+docker build -t ${DST_TAG} ${CONTEXT} \
         --build-arg FROM=${FROM_TAG}
 
 FROM_TAG=${DST_TAG}
-SRC_DIR=${SERVICE}/common/package
+CONTEXT=${SERVICE}/common/package
 DST_TAG=hinemos-${SERVICE}-base-${HINEMOS_MAJOR}.${HINEMOS_MINOR}:${JRE_TAG}
 echo ================================================================================
-echo ${SRC_DIR} "->" ${DST_TAG}
+echo ${CONTEXT} "->" ${DST_TAG}
 echo ================================================================================
-docker build -t ${DST_TAG} ${SRC_DIR} \
+docker build -t ${DST_TAG} ${CONTEXT} \
 	--build-arg FROM=${FROM_TAG} \
 	--build-arg MAJOR=${HINEMOS_MAJOR} \
 	--build-arg MINOR=${HINEMOS_MINOR} \
