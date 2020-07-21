f63a2aa63ca7533f3be48414ba4e274f90d5ec9f
froop <773445+froop@users.noreply.github.com>
2020-07-21 22:49:50 +0900

    rename SRC_DIR -> CONTEXT

--- base/build.sh
+++ base/build.sh
@@ -2,28 +2,33 @@
 
 . .env
 
-SRC_DIR=base/${OS}
+CONTEXT=base/${OS}
 DST_TAG=${OS}:latest
 echo ================================================================================
-echo ${SRC_DIR} "->" ${DST_TAG}
+echo Tag    : ${DST_TAG}
+echo Context: ${CONTEXT}
 echo ================================================================================
-docker build -t ${DST_TAG} ${SRC_DIR}
+docker build -t ${DST_TAG} ${CONTEXT}
 
 FROM_TAG=${DST_TAG}
-SRC_DIR=base/openjdk-redhat
+CONTEXT=base/openjdk-redhat
 DST_TAG=openjdk-${OS}:${JAVA_VER}
 echo ================================================================================
-echo ${SRC_DIR} "->" ${DST_TAG}
+echo Tag    : ${DST_TAG}
+echo From   : ${FROM_TAG}
+echo Context: ${CONTEXT}
 echo ================================================================================
-docker build -t ${DST_TAG} ${SRC_DIR} \
+docker build -t ${DST_TAG} ${CONTEXT} \
 	--build-arg FROM=${FROM_TAG} \
 	--build-arg VERSION=${JAVA_VER}
 
 FROM_TAG=${DST_TAG}
-SRC_DIR=base/hinemos-base
+CONTEXT=base/hinemos-base
 DST_TAG=hinemos-base:openjdk${JAVA_VER}-${OS}
 echo ================================================================================
-echo ${SRC_DIR} "->" ${DST_TAG}
+echo Tag    : ${DST_TAG}
+echo From   : ${FROM_TAG}
+echo Context: ${CONTEXT}
 echo ================================================================================
-docker build -t ${DST_TAG} ${SRC_DIR} \
+docker build -t ${DST_TAG} ${CONTEXT} \
 	--build-arg FROM=${FROM_TAG}
