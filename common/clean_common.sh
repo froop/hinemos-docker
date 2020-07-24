3a818875dd0346e12168d9275635e846db39cf25
froop <773445+froop@users.noreply.github.com>
2020-07-24 16:56:12 +0900

    rename OS -> OS_IMAGE

--- common/clean_common.sh
+++ common/clean_common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -u
 
 SERVICE=$1
-JRE_TAG=openjdk${JAVA_VER}-${OS}
+JRE_TAG=openjdk${JAVA_VER}-${OS_IMAGE}
 
 docker rmi hinemos-${SERVICE}-${HINEMOS_MAJOR}.${HINEMOS_MINOR}:${HINEMOS_TAG}
 docker rmi hinemos-${SERVICE}-base-${HINEMOS_MAJOR}.${HINEMOS_MINOR}:${JRE_TAG}
