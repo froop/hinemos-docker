0eae1d4c70b68154d7261fb7bbae709aae67dd74
froop <773445+froop@users.noreply.github.com>
2020-07-26 22:39:08 +0900

    refactor log message

--- base/build.sh
+++ base/build.sh
@@ -4,20 +4,20 @@
 
 CONTEXT=base/${OS_IMAGE}
 DST_TAG=${OS_IMAGE}:latest
-echo ================================================================================
-echo Tag    : ${DST_TAG}
-echo Context: ${CONTEXT}
-echo ================================================================================
+echo "================================================================================"
+echo "Tag    : ${DST_TAG}"
+echo "Context: ${CONTEXT}"
+echo "================================================================================"
 docker build -t ${DST_TAG} ${CONTEXT}
 
 FROM_TAG=${DST_TAG}
 CONTEXT=base/openjdk-redhat
 DST_TAG=openjdk-${OS_IMAGE}:${JAVA_VER}
-echo ================================================================================
-echo Tag    : ${DST_TAG}
-echo From   : ${FROM_TAG}
-echo Context: ${CONTEXT}
-echo ================================================================================
+echo "================================================================================"
+echo "Tag    : ${DST_TAG}"
+echo "From   : ${FROM_TAG}"
+echo "Context: ${CONTEXT}"
+echo "================================================================================"
 docker build -t ${DST_TAG} ${CONTEXT} \
 	--build-arg FROM=${FROM_TAG} \
 	--build-arg VERSION=${JAVA_VER}
@@ -25,10 +25,10 @@ docker build -t ${DST_TAG} ${CONTEXT} \
 FROM_TAG=${DST_TAG}
 CONTEXT=base/hinemos-base
 DST_TAG=hinemos-base:openjdk${JAVA_VER}-${OS_IMAGE}
-echo ================================================================================
-echo Tag    : ${DST_TAG}
-echo From   : ${FROM_TAG}
-echo Context: ${CONTEXT}
-echo ================================================================================
+echo "================================================================================"
+echo "Tag    : ${DST_TAG}"
+echo "From   : ${FROM_TAG}"
+echo "Context: ${CONTEXT}"
+echo "================================================================================"
 docker build -t ${DST_TAG} ${CONTEXT} \
 	--build-arg FROM=${FROM_TAG}
--- common/build_common.sh
+++ common/build_common.sh
@@ -6,22 +6,22 @@ JRE_TAG=openjdk${JAVA_VER}-${OS_IMAGE}
 FROM_TAG=hinemos-base:openjdk${JAVA_VER}-${OS_IMAGE}
 CONTEXT=${SERVICE}/base
 DST_TAG=hinemos-${SERVICE}-base:${JRE_TAG}
-echo ================================================================================
-echo Tag    : ${DST_TAG}
-echo From   : ${FROM_TAG}
-echo Context: ${CONTEXT}
-echo ================================================================================
+echo "================================================================================"
+echo "Tag    : ${DST_TAG}"
+echo "From   : ${FROM_TAG}"
+echo "Context: ${CONTEXT}"
+echo "================================================================================"
 docker build -t ${DST_TAG} ${CONTEXT} \
 	--build-arg FROM=${FROM_TAG}
 
 FROM_TAG=${DST_TAG}
 CONTEXT=${SERVICE}/${HINEMOS_MAJOR}/base
 DST_TAG=hinemos-${SERVICE}-base-${HINEMOS_MAJOR}:${JRE_TAG}
-echo ================================================================================
-echo Tag    : ${DST_TAG}
-echo From   : ${FROM_TAG}
-echo Context: ${CONTEXT}
-echo ================================================================================
+echo "================================================================================"
+echo "Tag    : ${DST_TAG}"
+echo "From   : ${FROM_TAG}"
+echo "Context: ${CONTEXT}"
+echo "================================================================================"
 docker build -t ${DST_TAG} ${CONTEXT} \
         --build-arg FROM=${FROM_TAG}
 
@@ -36,12 +36,12 @@ fi
 FROM_TAG=${DST_TAG}
 DOCKERFILE=${SERVICE}/common/DockerfilePackage
 DST_TAG=hinemos-${SERVICE}-base-${HINEMOS_MAJOR}.${HINEMOS_MINOR}:${JRE_TAG}
-echo ================================================================================
-echo Tag    : ${DST_TAG}
-echo From   : ${FROM_TAG}
-echo Context: ${CONTEXT}
-echo Dockerfile: ${DOCKERFILE}
-echo ================================================================================
+echo "================================================================================"
+echo "Tag    : ${DST_TAG}"
+echo "From   : ${FROM_TAG}"
+echo "Context: ${CONTEXT}"
+echo "Dockerfile: ${DOCKERFILE}"
+echo "================================================================================"
 docker build -t ${DST_TAG} -f ${DOCKERFILE} ${CONTEXT} \
 	--build-arg FROM=${FROM_TAG} \
 	--build-arg MAJOR=${HINEMOS_MAJOR} \
