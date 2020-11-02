9f5becd191d65033d30bbf5bc239bc327d03cf7b
froop <773445+froop@users.noreply.github.com>
2020-11-02 19:25:50 +0900

    https for WebClient

--- web/base/ssl/base-ssl-patch.sh
+++ web/base/ssl/base-ssl-patch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eu
 
+mv /tmp/patch/.keystore /opt/hinemos_web/
 sed -i -z 's|<Connector port="80"[^>]*>||' /opt/hinemos_web/conf/server.xml
 sed -i -z 's|<!--\(\s*<Connector port="443"[^>]*>\)\s*-->|\1|' /opt/hinemos_web/conf/server.xml
 
