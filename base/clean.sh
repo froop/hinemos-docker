ae07471f56fbb647cd13069df25a140f6d1cccfa
froop <773445+froop@users.noreply.github.com>
2021-04-24 22:04:10 +0900

    change: repogitory name

--- base/clean.sh
+++ base/clean.sh
@@ -2,6 +2,6 @@
 
 . .env
 
-docker rmi hinemos-base:openjdk${JAVA_VER}-${OS_IMAGE}
+docker rmi hinemos-base-openjdk${JAVA_VER}-${OS_IMAGE}
 docker rmi openjdk-${OS_IMAGE}:${JAVA_VER}
 docker rmi ${OS_IMAGE}
--- common/clean_common.sh
+++ common/clean_common.sh
@@ -3,7 +3,7 @@
 SERVICE=$1
 JRE_TAG=openjdk${JAVA_VER}-${OS_IMAGE}
 
-docker rmi hinemos-${SERVICE}-${HINEMOS_MAJOR}.${HINEMOS_MINOR}:${HINEMOS_TAG}
-docker rmi hinemos-${SERVICE}-base-${HINEMOS_MAJOR}.${HINEMOS_MINOR}:${JRE_TAG}
-docker rmi hinemos-${SERVICE}-base-${HINEMOS_MAJOR}:${JRE_TAG}
-docker rmi hinemos-${SERVICE}-base:${JRE_TAG}
+docker rmi hinemos-${SERVICE}-${HINEMOS_MAJOR}.${HINEMOS_MINOR}-${HINEMOS_TAG}
+docker rmi hinemos-${SERVICE}-base-${HINEMOS_MAJOR}.${HINEMOS_MINOR}-${JRE_TAG}
+docker rmi hinemos-${SERVICE}-base-${HINEMOS_MAJOR}-${JRE_TAG}
+docker rmi hinemos-${SERVICE}-base-${JRE_TAG}
