97450906e1feda231a9be26f75a2976ab6db5e52
froop <773445+froop@users.noreply.github.com>
2021-04-29 06:38:37 +0900

    refactor

--- manager/base/delete_SNMPTRAP_DEFAULT.sql
+++ manager/base/delete_SNMPTRAP_DEFAULT.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
+TRUNCATE TABLE setting.cc_monitor_trap_info CASCADE;
 DELETE FROM setting.cc_object_privilege WHERE object_id='SNMPTRAP_DEFAULT';
 DELETE FROM setting.cc_monitor_info WHERE monitor_id='SNMPTRAP_DEFAULT';
-TRUNCATE TABLE setting.cc_monitor_trap_info CASCADE;
 
 COMMIT;
