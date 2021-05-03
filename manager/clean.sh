f283d70958fb811a793888fb0861fecfd7fca82a
froop <773445+froop@users.noreply.github.com>
2021-05-03 19:21:46 +0900

    modify: hinemos-manager-restored

--- manager/clean.sh
+++ manager/clean.sh
@@ -8,5 +8,5 @@ export HINEMOS_MAJOR=$MANAGER_MAJOR
 export HINEMOS_MINOR=$MANAGER_MINOR
 export HINEMOS_TAG=$MANAGER_TAG
 
-common/clean_common.sh manager
 docker rmi frooprogrammer/hinemos-manager-restored-${MANAGER_MAJOR}.${MANAGER_MINOR}-openjdk${JAVA_VER}-${OS_IMAGE}
+common/clean_common.sh manager
