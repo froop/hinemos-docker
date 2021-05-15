5d0c83545b5803f415c6da117cd9ce9c8ca2a887
froop <773445+froop@users.noreply.github.com>
2021-05-15 23:47:50 +0900

    add: agent/4.1

---
+++ agent/build-4.1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eu
+
+. .env
+
+SERVICE=agent
+JRE_TAG=openjdk${JAVA_VER}-${OS_IMAGE}
+MAJOR_DIR=${SERVICE}/${AGENT_MAJOR}
+MINOR_DIR=${MAJOR_DIR}/${AGENT_MINOR}
+PACKAGE=hinemos_agent-${AGENT_MAJOR}.${AGENT_MINOR}_rhel5-7.tar.gz
+
+FROM_TAG=frooprogrammer/hinemos-base-${JRE_TAG}
+CONTEXT=${MINOR_DIR}/package
+DOCKERFILE=${MAJOR_DIR}/common/DockerfilePackage
+DST_TAG=frooprogrammer/hinemos-${SERVICE}-base-${AGENT_MAJOR}.${AGENT_MINOR}-${JRE_TAG}
+echo "================================================================================"
+echo "Tag    : ${DST_TAG}"
+echo "From   : ${FROM_TAG}"
+echo "Context: ${CONTEXT}"
+echo "Dockerfile: ${DOCKERFILE}"
+echo "================================================================================"
+docker build -t ${DST_TAG} -f ${DOCKERFILE} ${CONTEXT} \
+	--build-arg FROM=${FROM_TAG} \
+	--build-arg PACKAGE=${PACKAGE} \
+	--add-host=docker-host:${LOCAL_IP}
