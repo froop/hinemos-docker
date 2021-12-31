976285f9752c983bd408498e4eb217bdc67c352e
EC2 Default User <ec2-user@ip-172-30-3-10.ap-northeast-1.compute.internal>
2022-01-01 06:37:11 +0900

    add: manager/base/tool/backup-clean.sh

---
+++ manager/base/tool/backup-clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eu
+
+systemctl stop hinemos_manager
+systemctl start hinemos_pg
+export PGPASSWORD=hinemos
+
+/opt/hinemos/sbin/mng/hinemos_clear_tmp.sh
+/opt/hinemos/sbin/mng/hinemos_delete.sh -J
+/opt/hinemos/sbin/mng/hinemos_delete.sh -E
+/opt/hinemos/sbin/mng/hinemos_delete.sh -R
+/opt/hinemos/sbin/mng/hinemos_delete.sh -H
+/opt/hinemos/sbin/mng/hinemos_delete.sh -D
+/opt/hinemos/sbin/mng/hinemos_delete.sh -M
+/opt/hinemos/sbin/mng/hinemos_delete.sh -Z
+
+/opt/hinemos/sbin/mng/hinemos_backup.sh
+mv hinemos_pgdump.* hinemos_pgdump
