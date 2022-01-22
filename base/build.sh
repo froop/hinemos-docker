33698c6135cb693d18da8a540b7c86a21c6f0a4c
EC2 Default User <ec2-user@ip-172-30-3-10.ap-northeast-1.compute.internal>
2022-01-23 07:36:38 +0900

    modify: skip if docker image exists locally.

--- base/build.sh
+++ base/build.sh
@@ -3,6 +3,12 @@
 . .env
 SCRIPT_DIR=`dirname $0`
 
-${SCRIPT_DIR}/build-os.sh
-${SCRIPT_DIR}/build-java.sh
-${SCRIPT_DIR}/build-hinemos.sh
+if [[ "$(docker images -q ${REPOSITORY}/${OS_IMAGE})" == "" ]]; then
+	${SCRIPT_DIR}/build-os.sh
+fi
+if [[ "$(docker images -q ${REPOSITORY}/${JAVA_IMAGE}-${OS_IMAGE})" == "" ]]; then
+	${SCRIPT_DIR}/build-java.sh
+fi
+if [[ "$(docker images -q ${REPOSITORY}/hinemos-base-${JAVA_IMAGE}-${OS_IMAGE})" == "" ]]; then
+	${SCRIPT_DIR}/build-hinemos.sh
+fi
