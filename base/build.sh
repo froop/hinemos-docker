e42b117af42f98f62ae00ba214741c964230e685
EC2 Default User <ec2-user@ip-172-30-3-10.ap-northeast-1.compute.internal>
2022-01-30 15:06:24 +0900

    modify: skip build if image exists

--- base/build.sh
+++ base/build.sh
@@ -3,12 +3,12 @@
 . .env
 SCRIPT_DIR=`dirname $0`
 
-if [[ "$(docker images -q ${REPOSITORY}/${OS_IMAGE})" == "" ]]; then
-	${SCRIPT_DIR}/build-os.sh
-fi
-if [[ "$(docker images -q ${REPOSITORY}/${JAVA_IMAGE}-${OS_IMAGE})" == "" ]]; then
-	${SCRIPT_DIR}/build-java.sh
-fi
 if [[ "$(docker images -q ${REPOSITORY}/hinemos-base-${JAVA_IMAGE}-${OS_IMAGE})" == "" ]]; then
+	if [[ "$(docker images -q ${REPOSITORY}/${JAVA_IMAGE}-${OS_IMAGE})" == "" ]]; then
+		if [[ "$(docker images -q ${REPOSITORY}/${OS_IMAGE})" == "" ]]; then
+			${SCRIPT_DIR}/build-os.sh
+		fi
+		${SCRIPT_DIR}/build-java.sh
+	fi
 	${SCRIPT_DIR}/build-hinemos.sh
 fi
