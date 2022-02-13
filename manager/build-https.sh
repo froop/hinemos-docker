5e75c5606e1526dc2fbb12dc04ce440312540bde
EC2 Default User <ec2-user@ip-172-30-3-10.ap-northeast-1.compute.internal>
2022-02-13 10:44:40 +0900

    add: manager/https

---
+++ manager/build-https.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+. .env
+JRE_TAG=${JAVA_IMAGE}-${OS_IMAGE}
+
+DST_TAG=${REPOSITORY}/hinemos-manager-https-${JRE_TAG}:${MANAGER_MAJOR}.${MANAGER_MINOR}
+FROM_TAG=${REPOSITORY}/hinemos-manager-${JRE_TAG}:${MANAGER_MAJOR}.${MANAGER_MINOR}
+CONTEXT=manager/https
+echo "================================================================================"
+echo "Tag    : ${DST_TAG}"
+echo "From   : ${FROM_TAG}"
+echo "Context: ${CONTEXT}"
+echo "================================================================================"
+docker build -t ${DST_TAG} ${CONTEXT} \
+	--build-arg FROM=${FROM_TAG}
