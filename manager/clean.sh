f294950632a90a6c119b967b0572dd43fcaa56d4
EC2 Default User <ec2-user@ip-172-30-3-10.ap-northeast-1.compute.internal>
2022-05-15 16:01:46 +0900

    modify: docker rmi -f

--- manager/clean.sh
+++ manager/clean.sh
@@ -9,5 +9,5 @@ export HINEMOS_MAJOR=$MANAGER_MAJOR
 export HINEMOS_MINOR=$MANAGER_MINOR
 export HINEMOS_TAG=$MANAGER_TAG
 
-docker rmi ${REPOSITORY}/hinemos-manager-restored-${JAVA_IMAGE}-${OS_IMAGE}:${MANAGER_MAJOR}.${MANAGER_MINOR}
+docker rmi -f ${REPOSITORY}/hinemos-manager-restored-${JAVA_IMAGE}-${OS_IMAGE}:${MANAGER_MAJOR}.${MANAGER_MINOR}
 common/clean_common.sh manager
