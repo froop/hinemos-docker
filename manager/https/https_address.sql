25f7191923c84fb093c03a0ff472d892a5d14729
EC2 Default User <ec2-user@ip-172-30-3-10.ap-northeast-1.compute.internal>
2022-06-07 05:53:34 +0900

    add: monitor.customtrap.url

--- manager/https/https_address.sql
+++ manager/https/https_address.sql
@@ -8,4 +8,7 @@ UPDATE setting.cc_hinemos_property
 UPDATE setting.cc_hinemos_property
     SET value_string = 'https://0.0.0.0:8443', modify_user_id = 'hinemos', modify_datetime = EXTRACT(EPOCH FROM now()) * 1000
     WHERE property_key = 'infra.transfer.winrm.url';
+UPDATE setting.cc_hinemos_property
+    SET value_string = 'https://0.0.0.0:8082/', modify_user_id = 'hinemos', modify_datetime = EXTRACT(EPOCH FROM now()) * 1000
+    WHERE property_key = 'monitor.customtrap.url';
 COMMIT;
